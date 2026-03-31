6bd1221d7efb1ec701f9b2bb9d38ab50bd112328
princehw03 <princehw03@gmail.com>
2026-03-31 17:53:36 +0900

    [week02] 엠키/문현우 미션 제출

--- week03/app/src/main/java/kr/co/umc/nike/presentation/feat_buy/adapter/GoodsRVDecorator.kt
+++ week03/app/src/main/java/kr/co/umc/nike/presentation/feat_buy/adapter/GoodsRVDecorator.kt
@@ -14,6 +14,7 @@ class GoodsRVDecorator(private val spanCount: Int, private val space: Int): Recy
         val position = parent.getChildAdapterPosition(view)
         val column = position % spanCount // 현재 열 위치
 
+        outRect.left = if (column == spanCount - 1) space else 0
         outRect.right = if (column == spanCount - 1) 0 else space
 
     }
