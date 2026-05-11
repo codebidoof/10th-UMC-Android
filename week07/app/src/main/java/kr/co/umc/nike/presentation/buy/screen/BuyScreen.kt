848764d1de3f17254f3628e524cfbccf286d97dd
princehw03 <princehw03@gmail.com>
2026-05-11 17:18:47 +0900

    ✨ feat(ui): BuyScreen 상단 탭 바 구현
    - 가로 스크롤이 가능한 `TobBar` 컴포넌트 신규 생성
    - `BuyScreen` 내 탭 선택 상태 관리를 위한 `selectedTabIndexState` 및 탭 목록 정의
    - `ScrollableTabRow`를 사용하여 커스텀 인디케이터 및 탭 스타일(색상, 크기) 적용

--- week07/app/src/main/java/kr/co/umc/nike/presentation/buy/screen/BuyScreen.kt
+++ week07/app/src/main/java/kr/co/umc/nike/presentation/buy/screen/BuyScreen.kt
@@ -1,12 +1,33 @@
 package kr.co.umc.nike.presentation.buy.screen
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kr.co.umc.nike.presentation.buy.component.TobBar
 
 @Composable
 fun BuyScreen()  {
-    Text("구매하기 화면")
+    var selectedTabIndexState by remember { mutableIntStateOf(0) }
+    val tabs = listOf("전체", "Tops&T-Shirts", "sale")
+
+    Spacer(
+        modifier = Modifier
+            .height(16.dp)
+    )
+
+    TobBar(
+        currentTabIndex = selectedTabIndexState,
+        tabs = tabs,
+        onTabSelected = { selectedTabIndexState = it }
+    )
 }
 
 @Preview(showBackground = true)
