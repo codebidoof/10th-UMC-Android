142469b71280c85a15e71302fb87659e8155f427
princehw03 <princehw03@gmail.com>
2026-05-12 19:39:39 +0900

    ♻️ refactor(navigation): NavTransaction 내 애니메이션 함수명 변경 및 코드 정리
    - `fadeIn` 및 `fadeOut` 함수명을 각각 `fadeInTransition`, `fadeOutTransition`으로 변경
    - 함수명 변경에 따른 KDoc(주석) 설명 및 참조 업데이트
    - `fadeIn`, `fadeOut`을 직접 import 하여 코드 내 불필요한 전체 패키지 경로(Fully Qualified Name) 제거

--- week07/app/src/main/java/kr/co/umc/nike/navigation/util/NavTransaction.kt
+++ week07/app/src/main/java/kr/co/umc/nike/navigation/util/NavTransaction.kt
@@ -4,27 +4,29 @@ import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.navigation.NavBackStackEntry
 
 /**
  * Jetpack Compose Navigation 사용 시 화면 전환 애니메이션(Transition)을 정의하는 유틸리티 객체입니다.
  *
- * 이 객체는 [androidx.compose.animation.AnimatedContentTransitionScope] 내에서 사용할 수 있는 진입(Enter) 및 진출(Exit) 애니메이션을
- * 람다 형태로 제공하며, 기본적으로 [androidx.compose.animation.core.tween] 애니메이션 스펙을 사용합니다.
+ * 이 객체는 [AnimatedContentTransitionScope] 내에서 사용할 수 있는 진입(Enter) 및 진출(Exit) 애니메이션을
+ * 람다 형태로 제공하며, 기본적으로 [tween] 애니메이션 스펙을 사용합니다.
  *
  * 주요 기능:
- * - [fadeIn]: 화면이 나타날 때 점진적으로 투명도가 변하는 효과를 생성합니다.
- * - [fadeOut]: 화면이 사라질 때 점진적으로 투명도가 변하는 효과를 생성합니다.
+ * - [fadeInTransition]: 화면이 나타날 때 점진적으로 투명도가 변하는 효과를 생성합니다.
+ * - [fadeOutTransition]: 화면이 사라질 때 점진적으로 투명도가 변하는 효과를 생성합니다.
  *
  */
 object NavTransaction {
-    fun fadeIn(duration: Int = 200)
+    fun fadeInTransition(duration: Int = 200)
     : AnimatedContentTransitionScope<NavBackStackEntry>.() -> EnterTransition = {
-        androidx.compose.animation.fadeIn(animationSpec = tween(duration))
+        fadeIn(animationSpec = tween(duration))
     }
 
-    fun fadeOut(duration: Int = 200)
+    fun fadeOutTransition(duration: Int = 200)
             : AnimatedContentTransitionScope<NavBackStackEntry>.() -> ExitTransition = {
-        androidx.compose.animation.fadeOut(animationSpec = tween(duration))
+        fadeOut(animationSpec = tween(duration))
     }
 }
