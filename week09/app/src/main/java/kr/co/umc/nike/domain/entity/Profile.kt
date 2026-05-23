e88546bba54f38f680071e375d8747860d8a3949
princehw03 <princehw03@gmail.com>
2026-05-23 17:57:50 +0900

    remove: 9주차 폴더 삭제

--- week09/app/src/main/java/kr/co/umc/nike/domain/entity/Profile.kt
+++
@@ -1,8 +0,0 @@
-package kr.co.umc.nike.domain.entity
-
-data class Profile(
-    val id: Int,
-    val email: String,
-    val name: String,
-    val avatar: String,
-)
--- week09/app/src/main/java/kr/co/umc/nike/domain/repository/ProfileRepository.kt
+++
@@ -1,7 +0,0 @@
-package kr.co.umc.nike.domain.repository
-
-import kr.co.umc.nike.domain.entity.Profile
-
-interface ProfileRepository {
-    suspend fun getFollowingUsers(): Result<List<Profile>>
-}
