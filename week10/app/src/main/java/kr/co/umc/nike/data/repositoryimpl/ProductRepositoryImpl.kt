56ac9afe9ea196d9fd51162913c98df707570c58
princehw03 <princehw03@gmail.com>
2026-07-02 00:56:26 +0900

    ♻️ refactor(data): LocalDataSource 삭제 및 ProductDao 직접 참조로 변경
    - `LocalDataSource` 인터페이스 및 `LocalDataSourceImpl` 구현체 삭제
    - `DataSourceModule`에서 `LocalDataSource` 관련 Hilt 바인딩 설정 제거
    - `ProductRepositoryImpl`의 의존성을 `LocalDataSource`에서 `ProductDao`로 변경
    - 레포지토리 내 모든 데이터 접근 로직이 DAO를 직접 호출하도록 수정하여 데이터 계층 구조 단순화

--- week10/app/src/main/java/kr/co/umc/nike/data/repositoryimpl/ProductRepositoryImpl.kt
+++ week10/app/src/main/java/kr/co/umc/nike/data/repositoryimpl/ProductRepositoryImpl.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
-import kr.co.umc.nike.data.data_source.local.LocalDataSource
+import kr.co.umc.nike.data.data_source.local.room.dao.ProductDao
 import kr.co.umc.nike.data.mapper.DtoMapper.toDomain
 import kr.co.umc.nike.di.Dispatcher
 import kr.co.umc.nike.di.NikeDispatchers
@@ -13,32 +13,32 @@ import kr.co.umc.nike.domain.repository.ProductRepository
 import javax.inject.Inject
 
 class ProductRepositoryImpl @Inject constructor(
-    private val localDataSource: LocalDataSource,
+    private val productDao: ProductDao,
     @param:Dispatcher(NikeDispatchers.IO) private val ioDispatcher: CoroutineDispatcher,
 ) : ProductRepository {
     override fun getAllProducts(): Flow<List<ProductSummary>> {
-        return localDataSource.getAllProducts()
+        return productDao.getAllProducts()
             .map { entityList ->
                 entityList.map { it.toDomain() }
             }
     }
 
     override fun getProductsByWish(isWished: Boolean): Flow<List<ProductSummary>> {
-        return localDataSource.getProductsByWish(isWished)
+        return productDao.getProductsByWish(isWished)
             .map { entityList ->
                 entityList.map { it.toDomain() }
             }
     }
 
     override fun getProductsByNew(isNew: Boolean): Flow<List<ProductSummary>> {
-        return localDataSource.getProductsByNew(isNew)
+        return productDao.getProductsByNew(isNew)
             .map { entityList ->
                 entityList.map { it.toDomain() }
             }
     }
 
     override fun getProductsByCategory(categoryId: Int): Flow<List<ProductSummary>> {
-        return localDataSource.getProductsByCategory(categoryId)
+        return productDao.getProductsByCategory(categoryId)
             .map { entityList ->
                 entityList.map { it.toDomain() }
             }
@@ -46,7 +46,7 @@ class ProductRepositoryImpl @Inject constructor(
 
     override suspend fun updateWishStatus(id: Int) {
         withContext(ioDispatcher) {
-            localDataSource.updateWishStatus(id)
+            productDao.updateWishStatus(id)
         }
     }
 
