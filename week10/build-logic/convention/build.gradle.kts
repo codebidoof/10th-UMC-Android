1849bb1a1587c1a84ffd1fed174fa9238b482359
princehw03 <princehw03@gmail.com>
2026-07-02 01:34:23 +0900

    🔧 chore(build-logic): Convention plugin 자바 및 코틀린 컴파일 버전 설정
    - build-logic 프로젝트의 그룹명을 `kr.co.umc.nike.buildlogic`으로 설정
    - Java `sourceCompatibility` 및 `targetCompatibility`를 17 버전으로 지정
    - `KotlinCompile` 태스크의 `jvmTarget` 옵션을 17로 구성하여 컴파일러 버전 일치화

--- week10/build-logic/convention/build.gradle.kts
+++ week10/build-logic/convention/build.gradle.kts
@@ -1,6 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     `kotlin-dsl`
 }
+
+group = "kr.co.umc.nike.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
 
 dependencies {
     compileOnly(libs.android.gradlePlugin)
